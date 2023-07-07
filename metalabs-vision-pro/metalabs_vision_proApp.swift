792e25252dfa97a33b4c97dc80821559e069e0a2
Marco Fiorito <marcofiorito1@gmail.com>
2023-07-06 22:39:12 -0300

    first commit

---
+++ metalabs-vision-pro/metalabs_vision_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  metalabs_vision_proApp.swift
+//  metalabs-vision-pro
+//
+//  Created by Marco Fiorito on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct metalabs_vision_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
